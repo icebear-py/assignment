97d3d4f0bf4fb7784cbb6c466149c60e3f334862
Ansh <87576922+icebear-py@users.noreply.github.com>
2026-02-26 14:55:28 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use((req, res, next) => { res.setHeader('ngrok-skip-browser-warning', 'true'); next(); });
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
